aa7b2bcc79c9c2e6d73369d18dc7cc60e189357b
Kyle Kelley <rgbkrk@gmail.com>
2023-10-15 16:44:35 -0700

    starter typescript loading

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use deno_core::op;
 use deno_core::Extension;
 use std::rc::Rc;
 
+use deno_ast::MediaType;
+use deno_ast::ParseParams;
+use deno_ast::SourceTextInfo;
+use deno_core::futures::FutureExt;
+
 #[op]
 async fn op_read_file(path: String) -> Result<String, AnyError> {
     let contents = tokio::fs::read_to_string(path).await?;
@@ -25,6 +30,73 @@ fn op_remove_file(path: String) -> Result<(), AnyError> {
 async fn op_fetch(url: String) -> Result<String, AnyError> {
     let body = reqwest::get(url).await?.text().await?;
     Ok(body)
+}
+
+struct TsModuleLoader;
+
+impl deno_core::ModuleLoader for TsModuleLoader {
+    fn resolve(
+        &self,
+        specifier: &str,
+        referrer: &str,
+        _kind: deno_core::ResolutionKind,
+    ) -> Result<deno_core::ModuleSpecifier, deno_core::error::AnyError> {
+        deno_core::resolve_import(specifier, referrer).map_err(|e| e.into())
+    }
+
+    fn load(
+        &self,
+        module_specifier: &deno_core::ModuleSpecifier,
+        _maybe_referrer: Option<deno_core::ModuleSpecifier>,
+        _is_dyn_import: bool,
+    ) -> std::pin::Pin<Box<deno_core::ModuleSourceFuture>> {
+        let module_specifier = module_specifier.clone();
+
+        async move {
+            let path = module_specifier.to_file_path().unwrap();
+
+            let media_type = MediaType::from_path(&path);
+            let (module_type, should_transpile) = match MediaType::from_path(&path) {
+                MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
+                    (deno_core::ModuleType::JavaScript, false)
+                }
+                MediaType::Jsx => (deno_core::ModuleType::JavaScript, true),
+                MediaType::TypeScript
+                | MediaType::Mts
+                | MediaType::Cts
+                | MediaType::Dts
+                | MediaType::Dmts
+                | MediaType::Dcts
+                | MediaType::Tsx => (deno_core::ModuleType::JavaScript, true),
+                MediaType::Json => (deno_core::ModuleType::Json, false),
+                _ => panic!("Unknown extension {:?}", path.extension()),
+            };
+
+            let code = std::fs::read_to_string(&path)?;
+            let code = if should_transpile {
+                let parsed = deno_ast::parse_module(ParseParams {
+                    specifier: module_specifier.to_string(),
+                    text_info: SourceTextInfo::from_string(code),
+                    media_type,
+                    capture_tokens: false,
+                    scope_analysis: false,
+                    maybe_syntax: None,
+                })?;
+                parsed.transpile(&Default::default())?.text
+            } else {
+                code
+            };
+
+            let module = deno_core::ModuleSource {
+                code: code.into(),
+                module_type,
+                module_url_specified: module_specifier.into(),
+                module_url_found: module_specifier.to_string().into(),
+            };
+            Ok(module)
+        }
+        .boxed_local()
+    }
 }
 
 async fn fun_js(file_path: &str) -> Result<(), AnyError> {
